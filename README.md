cfb344d81208df44d8314abaebbf1a5e9f1c62e7
Gas Com IT <99112362+GasComIT@users.noreply.github.com>
2022-03-08 13:39:37 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
-## Rubby 🐶
-Advanced WhatsApp bot written in Python by Alison Parker
-Bot in not yet published, so don't even think about it.
-```python
-from your_brains import *
-import patience
-from patience import to_chill as Relax
-     Relax{
-       Wait & watch_it:
-Happen{
-  This_is:
-  Rubby(🐶)
-           }
-By Alison.append(Royce Bob El Willie)
-  Break:
-        }
-print("Hello there and nice to meet you")
-```
+# Whatsapp-Radiologist
+A chatbot built in python using Selenium module that can predict lung diseases through Xray. I'm using the ChexNet's amazing model to accomplish this task accurately.
+
+## Running the bot
+1. If you want to test the image captioning bot feature, make sure you have put the Azure API key in loreal.py to use the image captioning bot feature.
+2. Run the command <code>python3 bot.py</code>
+3. Scan the QR code with your phone to start whatsapp web on browser that pops up. (You may have to configure selenium geckodriver depending on the OS you are using. Specify geckodriver.exe's path in selenium inside bot.py file)
+4. Ask your friend to send <b>activate bot</b> to you.
+5. Now he can either send <b>show news</b> or send an Xray to start the diagnosis.
+
+Feel free to improve the code and add features.
 
 
-### ▽⬡ 𝐆𝐚𝐬𝐂𝐨𝐦 𝐈𝐓 ⬡▽ By Royce Bob```ﾉ 
-keep waiting as you view:
+Currently the chatbot is only capable of the following tasks:
 
-My latest Release: [Qtell](https://github.com/qtell)
+
+## 1. Lung Xray Diagnosis
+Using chexnet model. refer: https://github.com/brucechou1983/CheXNet-Keras for keras chexnet
+
+and https://arxiv.org/pdf/1711.05225.pdf for the original paper.
+
+
+## 2. Image Captioning Bot
+A bot that captions the image sent to you using Microsoft's Azure platform. You will need an API key from Azure to run this. If you dont have one, you can make your own "caption" function and play around the image.
+
+## 3.. News show
+Any message you will get after you run the bot having the word "show news" in it will be treated as a request for latest headlines. The bot then fetches the latest news. 
--- bot.py
+++ bot.py
@@ -1,10 +1,86 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import requests
-from time import sleep
+import pyautogui
+from loreal import caption
+import os
+from chexnet import predict
+
+
+
+def radio(images,action):
+	image = images[-1]
+	text_box = browser.find_element_by_class_name("_3u328")
+	response = "Diagnosing... Please wait "+name+".\n"
+	text_box.send_keys(response)
+	action = webdriver.common.action_chains.ActionChains(browser)
+	print('New image found, saving')
+	sleep(5)
+	save(image,action)
+	try:
+		preds = predict(path)
+		c = 1
+		text_box.send_keys('Top 3 probabilities :\n')
+		for pred in preds:
+			text_box.send_keys(str(c)+'. '+pred+'\n')#+' with probability:'+pred[1]+'\n')
+			c += 1
+	except Exception as e:
+		print(e)
+		text_box.send_keys('The image you sent is either too hard for me to understand or it is a server issue, you can try sending me image again\n')
+	os.remove(path) # Cleanup
+
+
+options = Options()
+# options.set_headless(headless=True)
+dirpath = os.getcwd()
+browser = webdriver.Chrome(executable_path = dirpath+"/chromedriver")#firefox_options=options)
+url = "https://news.google.com/?hl=en-IN&gl=IN&ceid=IN:en"
+root = "https://news.google.com"
+browser.get('https://web.whatsapp.com')
+print('Scan and get started')
+sleep(10)
+
+path = '/Users/Karan/Downloads/WhatsappTest.jpg' #Your Downloads Path Here (Keep WhatsappTest.jpg as it is)
+
+
+def save(image,action):
+	'''To save the image'''
+	action.context_click(image).perform()
+	pyautogui.typewrite(['down','down','enter'])
+	sleep(1.5)
+	file = 'WhatsappTest.jpg'
+	pyautogui.typewrite(file)
+	sleep(0.5)
+	pyautogui.typewrite(['enter'])
+	sleep(3)
+
+
+
+def getCaption(images,action):
+	image = images[-1]
+	text_box = browser.find_element_by_class_name("_3u328")
+	response = "Analyzing... Please wait "+name+".\n"
+	text_box.send_keys(response)
+	action = webdriver.common.action_chains.ActionChains(browser)
+	print('New image found, saving')
+	sleep(4)
+	save(image,action)
+	try:
+		cap = caption(path)
+		text_box.send_keys('Prediction : "'+cap.capitalize()+'"\n')
+	except Exception as e:
+		print(e)
+		text_box.send_keys('The image you sent is either too hard for me to understand or it is a server issue, you can try sending me image again\n')
+	os.remove(path) # Cleanup
+
 
 def getNews():
-	text_box = browser.find_element_by_class_name("_2S1VP")
+	text_box = browser.find_element_by_class_name("_3u328")
 	response = "Let me fetch and send top 5 latest news:\n"
 	text_box.send_keys(response)
 	soup = BeautifulSoup(requests.get(url).content, "html5lib")
@@ -15,44 +91,39 @@ def getNews():
 	for i in range(5):
 		text_box.send_keys(news[i] + "==>" + links[i] + "\n")
 
-bot_users = {} # A dictionary that stores all the users that sent activate bot 
+
+bot_users = {}
+
+
 while True:
-	unread = browser.find_elements_by_class_name("OUeyt") # The green dot tells us that the message is new
-	name,message  = '',''
+	unread = browser.find_elements_by_class_name("P6z4j")
 	if len(unread) > 0:
-		ele = unread[-1]
+		ele = unread[0]
 		action = webdriver.common.action_chains.ActionChains(browser)
-		action.move_to_element_with_offset(ele, 0, -20) # move a bit to the left from the green dot
-        
-        # Clicking couple of times because sometimes whatsapp web responds after two clicks
-		try:
-			action.click()
-			action.perform()
-			action.click()
-			action.perform()
-		except Exception as e:
-			pass
-		try:
-			name = browser.find_element_by_class_name("_2zCDG").text  # Contact name
-			message = browser.find_elements_by_class_name("vW7d1")[-1]  # the message content
-			if 'activate bot' in message.text.lower():
-				if name not in bot_users:
-					bot_users[name] = True
-					text_box = browser.find_element_by_class_name("_2S1VP")
-					response = "Hi "+name+". Aravind's Bot here :). Now I am activated for you\n"
-					text_box.send_keys(response)
+		action.move_to_element_with_offset(ele, 0, -20)
+		action.click()
+		action.perform()
+		sleep(2)
+		name = browser.find_element_by_class_name("_1lpto").text
+		print(name)
+		message = browser.find_elements_by_class_name("FTBzM")[-1]
+		images = message.find_elements_by_class_name("_18vxA")
+		if 'activate bot' in message.text.lower():
+			if name not in bot_users:
+				bot_users[name] = True
+				text_box = browser.find_element_by_class_name("_3u328")
+				response = "Hi "+name+". Aravind's Bot here :). Now I am activated for you\n"
+				text_box.send_keys(response)
+		if name in bot_users:
+			if len(images) != 0:
+				# replace with radio
+				getCaption(images, action)
+			elif 'show' in message.text.lower() and 'news' in message.text.lower() and 'don' not in message.text.lower():
+				getNews()
+		if 'deactivate' in message.text.lower():
 			if name in bot_users:
-				if 'show' in message.text.lower() and 'news' in message.text.lower():
-					getNews()
-			if 'deactivate' in message.text.lower():
-				if name in bot_users:
-					text_box = browser.find_element_by_class_name("_2S1VP")
-					response = "Bye "+name+".\n"
-					text_box.send_keys(response)
-					del bot_users[name]
-		except Exception as e:
-			print(e)
-			pass
-	sleep(2) 
-#A 2 second pause so that the program doesn't run too fast
-#view raw https://github.com/GasComIT/NewsBot hosted with ❤ by GitHub
+				text_box = browser.find_element_by_class_name("_3u328")
+				response = "Bye "+name+".\n"
+				text_box.send_keys(response)
+				del bot_users[name]
+	sleep(2)
